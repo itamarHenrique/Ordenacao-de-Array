3662738023757ad0b9546e295fcfc1c3794362c7
itamarHenrique <itamarhjp1@gmail.com>
2023-12-07 15:22:47 -0300

    acertando a linha do for

--- src/ordenação/OrdenacaoInt.java
+++ src/ordenação/OrdenacaoInt.java
@@ -26,9 +26,9 @@ public class OrdenacaoInt {
 		for (int i = 0; i < array.length; i++) {
 			for (int j = i + 1; j < array.length; j++) {
 				if(array[i] > array[j]) {
-					int aux = array[i];
-					array[i] = array[j];
-					array[j] = aux;
+				 int aux = array[i];
+				 array[i] = array[j];
+				 array[j] = aux;
 			}
 		}
 		
